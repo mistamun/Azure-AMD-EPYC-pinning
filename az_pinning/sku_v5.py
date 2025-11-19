f5a6e5f4bc9f0d87f5276b34fdd7fe9fcfe35de3
pedro.neto <pedro.neto@astonmartinf1.com>
2025-11-19 13:46:42 +0000

    v5 has 4 sockets

--- az_pinning/sku_v5.py
+++ az_pinning/sku_v5.py
@@ -4,6 +4,9 @@ from .sku_base import SKU
 
 class HB368v5(SKU):
     def __init__(self, cpuCores=None):
-        super().__init__([GA9V64H(), GA9V64H()])
+        super().__init__([
+            GA9V64H(), GA9V64H(),
+            GA9V64H(), GA9V64H(),
+            ])
         if cpuCores is not None:
             self.subscribe(cpuCores)
